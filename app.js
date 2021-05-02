e4deb258c8765225784b5df6c110c2500bb7e3ec
alannanoguchi <alanna.noguchi@gmail.com>
2021-05-01 18:33:04 -0700

    Users can create new channels and join

--- app.js
+++ app.js
@@ -27,3 +27,13 @@ app.get('/', (req, res) => {
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
 })
+
+const io = require('socket.io')(server);
+let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []};
+
+io.on("connection", (socket) => {
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
